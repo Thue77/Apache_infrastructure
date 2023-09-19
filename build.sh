2deddf85ebdfad7976beb908ef326f038d82528c
Thomas Sørensen <thom199b@gmail.com>
2023-09-19 17:33:11 +0000

    no owner for tar and build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+# Set input variables
+SPARK_VERSION="3.3.3"
+HADOOP_VERSION="3"
+JUPYTERLAB_VERSION="4.0.5"
+
+
+# Build images
+
+docker build -f base/Dockerfile -t cluster-base .
+
+docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg hadoop_version="${HADOOP_VERSION}" -f spark-base/Dockerfile -t spark-base .
+
+docker build -f spark-master/Dockerfile -t spark-master . 
+
+docker build -f spark-worker/Dockerfile -t spark-worker .
+
+docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" -f jupyterlab/Dockerfile -t jupyterlab .
+
+docker build -f airflow/Dockerfile -t airflow .
