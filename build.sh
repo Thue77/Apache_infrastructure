2358399e338daac36aa49358a363f30bbfd18eb3
Thomas Sørensen <thom199b@gmail.com>
2023-09-20 06:04:34 +0000

    .

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ JUPYTERLAB_VERSION="4.0.5"
 
 # Build images
 
-docker build -f base/Dockerfile -t cluster-base .
+docker build -f base/Dockerfile -t cluster-base:latest .
 
-docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg hadoop_version="${HADOOP_VERSION}" -f spark-base/Dockerfile -t spark-base .
+docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg hadoop_version="${HADOOP_VERSION}" -f spark-base/Dockerfile -t spark-base:latest .
 
-docker build -f spark-master/Dockerfile -t spark-master . 
+# docker build -f spark-master/Dockerfile -t spark-master . 
 
-docker build -f spark-worker/Dockerfile -t spark-worker .
+# docker build -f spark-worker/Dockerfile -t spark-worker .
 
-docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" -f jupyterlab/Dockerfile -t jupyterlab .
+# docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" -f jupyterlab/Dockerfile -t jupyterlab .
 
-docker build -f airflow/Dockerfile -t airflow .
+# docker build -f airflow/Dockerfile -t airflow .
