5ea7e1d970ada13889cd7b2385656d491ec56d65
Thomas Sørensen <thom199b@gmail.com>
2023-09-23 12:03:55 +0000

    modules for running a spark job

---
+++ src/jupyter/scripts/cdk/common_modules/utility/spark_session_builder.py
@@ -0,0 +1,26 @@
+from typing import Protocol,List
+from pyspark.sql import SparkSession
+
+class SparkConfig(Protocol):
+    '''Interface for spark config
+    '''
+    configurations: dict
+
+class SparkSessionBuilder:
+    '''Class to build SparkSession
+    '''
+
+    def __init__(self, app_name: str, master: str, spark_config: SparkConfig):
+        self.app_name = app_name
+        self.master = master
+        self.spark_config = spark_config
+    
+    def build(self):
+        spark_session = (
+            SparkSession.builder.appName(self.app_name)
+            .master(self.master)
+            .config("spark.executor.memory", "512m")
+        )
+        for key, value in self.spark_config.configurations.items():
+            spark_session = spark_session.config(key, value)
+        return spark_session.getOrCreate()
