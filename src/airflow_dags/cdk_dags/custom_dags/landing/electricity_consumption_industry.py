67a792b07906f2037a49c5ec1985f6f77476c1ed
Thomas Sørensen <thom199b@gmail.com>
2023-09-25 17:27:13 +0000

    add dataset

--- src/airflow_dags/cdk_dags/custom_dags/landing/electricity_consumption_industry.py
+++ src/airflow_dags/cdk_dags/custom_dags/landing/electricity_consumption_industry.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from pathlib import Path
 import sys
 import tempfile
 import time
@@ -12,19 +13,27 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.python import ExternalPythonOperator, PythonVirtualenvOperator, is_venv_installed
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.datasets import Dataset
 from datetime import datetime
 log = logging.getLogger(__name__)
 
-test = {
+spark_conf = {
     "spark.jars.packages": 'org.apache.hudi:hudi-spark3.3-bundle_2.12:0.13.1,org.apache.hadoop:hadoop-azure:3.3.3'
 }
 
+landing_dataset = Dataset(Path(__file__).stem)
+
 with DAG("landing_electricity_consumption_industry",start_date=datetime(2023,5,27),
-    schedule_interval="@daily", catchup=False) as dag:
-    run_spark_job = SparkSubmitOperator(
-        task_id = "run_spark_job",
+    schedule_interval="@daily", 
+    description = "Spark job to load electricity consumption by industry from Energi Data Service API(https://www.energidataservice.dk/) to landing",
+    catchup=False) as dag:
+
+
+    api_to_landing = SparkSubmitOperator(
+        task_id = "api_to_landing",
         application = "/opt/bitnami/spark/apps/landing/electricity_consumption_industry.py",
         conn_id = "spark_conn",
         verbose = True,
-        conf = test
+        outlets = [landing_dataset],
+        conf = spark_conf
     )
