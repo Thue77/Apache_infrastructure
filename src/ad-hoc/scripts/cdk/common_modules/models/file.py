5e49110a4db1fe3835bfda1cf5487106237a109b
Thomas Thue Sørensen <thomas@thuesoerensen.dk>
2024-01-10 09:29:12 +0100

    remove module code

--- src/ad-hoc/scripts/cdk/common_modules/models/file.py
+++
@@ -1,9 +0,0 @@
-import dataclasses
-from typing import List, Tuple, Any
-
-@dataclasses.dataclass
-class File:
-    name: str
-    path: str
-    type: str
-    partitions: List[Tuple[str,Any]] = None
